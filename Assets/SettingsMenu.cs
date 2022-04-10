556d183ce39683482e197c7cb9810f06807012c1
Mathéo <matheo.destampes@e-artsup.net>
2022-04-10 17:35:30 +0200

    Barre de son et options menu

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // Start is called before the first frame update
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
