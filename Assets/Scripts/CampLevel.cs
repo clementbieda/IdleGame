5a3e91fe434eb761111a42e6428daa761d4c4cbd
Mathéo <matheo.destampes@e-artsup.net>
2022-04-12 10:06:28 +0200

    Push pour réparer lia

--- Assets/Scripts/CampLevel.cs
+++ Assets/Scripts/CampLevel.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class CampLevel : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public Sprite newSprite;
-    public Sprite[] spriteArray;
 
-    void ChangeSprite()
-    {
-        spriteRenderer.sprite = newSprite; 
-    }
+    public Sprite[] spriteArray;
+    public int state;
 
     void Start()
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteArray = Resources.LoadAll<Sprite>("BirdHeroSprite");
+
+    }
+
+    public void ChangeSprite() 
+    { 
+        spriteRenderer.sprite = spriteArray[0]; 
     }
 
 
