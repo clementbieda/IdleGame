38547624292abb9649f0ec0e0d8f763b0fce0e87
Mathéo <matheo.destampes@e-artsup.net>
2022-04-14 10:06:10 +0200

    Menu Modification + new script

---
+++ Assets/Scripts/Save & Load/SaveLoad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoad : MonoBehaviour
+{
+
+    public TextMeshProUGUI currentMoney;
+
+
+    public void clickSaveButton()
+    {
+        PlayerPrefs.SetString("money", currentMoney.text);
+        Debug.Log("Tu as " + PlayerPrefs.GetString("money"));
+    }
+
+    
+}
